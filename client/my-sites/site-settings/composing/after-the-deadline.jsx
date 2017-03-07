a5eca221839fc18dc6ff208f023bfef12e65fc7c
oskosk <oskosk@gmail.com>
2017-03-07 14:13:55 -0300

    Replace usage of an <hr> for site-settings styles related to fieldsets

--- client/my-sites/site-settings/composing/after-the-deadline.jsx
+++ client/my-sites/site-settings/composing/after-the-deadline.jsx
@@ -180,9 +180,8 @@ class AfterTheDeadline extends Component {
 		} = this.props;
 
 		return (
-			<FormFieldset>
+			<FormFieldset className="site-settings__has-divider is-top-only">
 				<QueryJetpackConnection siteId={ selectedSiteId } />
-
 				<div className="composing__info-link-container site-settings__info-link-container">
 					<InfoPopover position={ 'left' }>
 						<ExternalLink href={ 'https://jetpack.com/support/spelling-and-grammar/' } target="_blank">
