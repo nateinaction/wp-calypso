c3634d295b555674f83c8a4bee635f0a4b17d032
Donna Peplinskie <donnapep@gmail.com>
2017-03-10 07:32:54 -0500

    Switch to using data layer for updating video poster

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -3,15 +3,17 @@
  */
 import { mergeHandlers } from 'state/data-layer/utils';
 import plans from './plans';
-import sites from './sites';
 import read from './read';
+import sites from './sites';
 import timezones from './timezones';
+import videos from './videos';
 
 export const handlers = mergeHandlers(
 	plans,
-	sites,
 	read,
+	sites,
 	timezones,
+	videos,
 );
 
 export default handlers;
